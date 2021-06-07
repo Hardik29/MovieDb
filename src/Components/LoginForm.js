1009a0d1a8439029e983505604fd172eb458982e
Hardik singh <singhhardik14@gmail.com>
2021-06-08 01:38:40 +0530

    added validation using joi and minor improvments

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import LoginComponent from './LoginComponent';
 
 export default class LoginForm extends Component {
@@ -9,7 +10,7 @@ export default class LoginForm extends Component {
 
   handleChange = (e) => {
        const errors = {...this.state.errors}
-       const errorsMessage = this.validateProperty(e.currentTarget);
+       const errorsMessage = this.validateProperty(e.currentTarget.name,e.currentTarget.value);
        if(errorsMessage) errors[e.currentTarget.name] = errorsMessage;
        else delete errors[e.currentTarget.name]
        
@@ -17,24 +18,45 @@ export default class LoginForm extends Component {
        account[e.currentTarget.name] = e.currentTarget.value
        this.setState({account, errors})
   }
+  schema ={ 
+    name: Joi.string().required().label("username"),
+    password: Joi.string().required(),
+  }
 
-  validateProperty = (input) => {
-    if(input.name === 'name'){
+  validateProperty = (input, value) => {
+    const obj = { [input]: value}
+    const schema = { [input] : this.schema[input]}
+    const result = Joi.validate(obj, schema)
+    if(!result.error) return null
+    return result.error.details[0].message
+    /*if(input.name === 'name'){
       if(input.value.trim() === '') {return "username is required";}
     }
     if(input.name === 'password'){
       if(input.value.trim() === '') {return "password is required";}
-    }
+    }*/
   }
 
   validate= () =>{
+    const result = Joi.validate(this.state.account, this.schema, {abortEarly:false})
+    console.log(result)
     const errors ={}
-    if(this.state.account.name.trim() === "")
+    if(!result.error) return null
+    if(result.error){
+      for(let obj in result.error.details){
+        const value = result.error.details[obj]
+        errors[value.path[0]] = value.message
+    }
+      return errors
+    }
+
+
+    /*if(this.state.account.name.trim() === "")
     errors.name = "username is required";
     if(this.state.account.password.trim() === "")
     errors.password = "paswword is required";
 
-    return (Object.keys(errors).length === 0 ? null : errors )
+    return (Object.keys(errors).length === 0 ? null : errors )*/
   }
   handleSubmit = (e) => {
     e.preventDefault();
@@ -50,6 +72,7 @@ export default class LoginForm extends Component {
          <div className="md:flex md:items-center">
             <div className="md:w-2/3">
               <button
+                disabled={this.validate()}
                 className="shadow bg-purple-500 hover:bg-purple-400  text-white font-bold py-2 px-4 rounded"
                 type="submit"
                 >
